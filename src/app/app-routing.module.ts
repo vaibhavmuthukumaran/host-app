c157a2c8938a8b13aab3fa6a43ab6cd608e9940f
Vaibhav Muthukumaran <vaibhavmuthukumaran@gmail.com>
2024-11-19 17:12:40 +0530

    Basic Host Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'orders',
+    loadChildren: () => {
+      return loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4001/remoteEntry.js',
+        exposedModule: './OrderModule',
+      })
+        .then((m) => m.OrderModule)
+        .catch((e) => console.log(e));
+    },
+  },
+  {
+    path: 'warehouse',
+    loadChildren: () => {
+      return loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4002/remoteEntry.js',
+        exposedModule: './WarehouseModule',
+      }).then((m) => m.WarehouseModule);
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  remotes: {
+    // "mfe1": "http://localhost:3000/remoteEntry.js",
+    mfe1: "mfe1@http://localhost:4001/remoteEntry.js",
+    mfe2: "mfe2@http://localhost:4002/remoteEntry.js",
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
